dd8223f628483cb029e6e7a36f39a87589a24bef
Gachenge <gachenge1@gmail.com>
2023-06-28 12:05:32 +0300

    node modules

--- 0x01-ES6_promise/1-main.js
+++ 0x01-ES6_promise/1-main.js
@@ -1,5 +1,4 @@
-import guardrail from './9-try';
-import divideFunction from './8-try';
+import getFullResponseFromAPI from './1-promise';
 
-console.log(guardrail(() => divideFunction(10, 2)));
-console.log(guardrail(() => divideFunction(10, 0)));
+console.log(getFullResponseFromAPI(true));
+console.log(getFullResponseFromAPI(false));
--- 0x01-ES6_promise/README.md
+++
@@ -1,2 +0,0 @@
-Promises. Just like a politicians, they can be fulfilled or broken
-we need to handle either case
