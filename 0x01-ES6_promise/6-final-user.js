21fed0b4909dc0aba2480e7a15242565a899a7ae
Gachenge <gachenge1@gmail.com>
2023-06-28 12:56:10 +0300

    typo in final-user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,9 @@
-/* eslint-disable no-unused-vars */
-
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName);
-  const photo = uploadPhoto(filename);
+  const photo = uploadPhoto(fileName);
 
   return Promise.allSettled([user, photo]).then((resp) => {
     const myarr = [];
