04040ab6baf4a13b1631fcb2ffedf2abdefb221c
Gachenge <gachenge1@gmail.com>
2023-08-22 10:26:03 +0300

    list correction

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 
 function countStudents(path) {
   let num = 0;
-  const students = [];
-  const student = [];
+  const studentcs = [];
+  const studentswe = [];
 
   return new Promise((resolve, reject) => {
     fs.readFile(path, (error, data) => {
@@ -14,15 +14,15 @@ function countStudents(path) {
         for (let i = 0; i < lines.length; i += 1) {
           const line = lines[i].toString().split(',');
           if (line[3] === 'CS') {
-            students.push(line[0]);
+            studentcs.push(line[0]);
           } else if (line[3] === 'SWE') {
-            student.push(line[0]);
+            studentswe.push(line[0]);
           }
           num = i - 1;
         }
         console.log(`Number of students: ${num}`);
-        console.log(`Number of students in CS: ${students.length}. List: ${students}`);
-        console.log(`Number of students in SWE: ${student.length}. List: ${student}`);
+        console.log(`Number of students in CS: ${studentcs.length}. List: ${studentcs.join(', ')}`);
+        console.log(`Number of students in SWE: ${studentswe.length}. List: ${studentswe.join(', ')}`);
       }
       resolve(data);
     });
--- 0x05-Node_JS_basic/main.js
+++ 0x05-Node_JS_basic/main.js
@@ -1,3 +1,10 @@
-const countStudents = require('./2-read_file');
+const countStudents = require('./3-read_file_async');
 
-countStudents('database.csv');
+countStudents('database.csv')
+  .then(() => {
+    console.log('Done!');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+console.log('After!');
