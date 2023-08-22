a60441375e06fca7c07be5adf946f0a4d132d671
Gachenge <gachenge1@gmail.com>
2023-08-22 10:07:58 +0300

    eslint correction

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,14 +2,14 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name}\n`);
+    const name = data.toString();
+    process.stdout.write(`Your name is: ${name}`);
     process.exit();
   });
 } else {
   process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name}\n`);
+    const name = data.toString();
+    process.stdout.write(`Your name is: ${name}`);
   });
 
   process.on('exit', () => {
