25b569d3d136151609b1e764f1e3baf5e38a55f2
Gachenge <gachenge1@gmail.com>
2023-08-22 10:21:47 +0300

    list correction

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 
 function countStudents(path) {
   let num = 0;
-  const students = [];
-  const student = [];
+  const studentswe = [];
+  const studentcs = [];
 
   try {
     const data = fs.readFileSync(path, 'utf-8');
@@ -11,18 +11,18 @@ function countStudents(path) {
     for (let i = 0; i < lines.length; i += 1) {
       const line = lines[i].toString().split(',');
       if (line[3] === 'CS') {
-        students.push(line[0]);
+        studentcs.push(line[0]);
       } else if (line[3] === 'SWE') {
-        student.push(line[0]);
+        studentswe.push(line[0]);
       }
       num = i - 1;
     }
   } catch (error) {
-    throw Error('Cannot load the database');
+    throw new Error('Cannot load the database');
   }
   console.log(`Number of students: ${num}`);
-  console.log(`Number of students in CS: ${students.length}. List: ${students}`);
-  console.log(`Number of students in SWE: ${student.length}. List: ${student}`);
+  console.log(`Number of students in CS: ${studentcs.length}. List: ${studentcs.join(', ')}`);
+  console.log(`Number of students in SWE: ${studentswe.length}. List: ${studentswe.join(', ')}`);
 }
 
 module.exports = countStudents;
