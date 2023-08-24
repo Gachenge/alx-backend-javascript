e0e825b459920639f1e9b8da07d2f8c390686912
Gachenge <gachenge1@gmail.com>
2023-08-24 19:46:37 +0300

    corrections for package.json

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,46 +1,45 @@
 const { expect } = require('chai');
 const request = require('request');
-const { response } = require('./api');
 const { json } = require('body-parser');
 
 describe('api tests', () => {
     it('should test get / returns the correct response', (done) => {
-        request.get('http://localhost:7865', (error, response, body) => {
-            expect(response.statusCode).to.be.equal(200);
+        request.get('http://localhost:7865', (_error, resp, body) => {
+            expect(resp.statusCode).to.be.equal(200);
             expect(body).to.be.equal('Welcome to the payment system');
             done();
         })
     })
     it('validate if id is a number', (done) => {
-        request.get('http://localhost:7865/cart/9', (error, response, body) => {
-            expect(response.statusCode).to.be.equal(200);
+        request.get('http://localhost:7865/cart/9', (_error, resp, body) => {
+            expect(resp.statusCode).to.be.equal(200);
             expect(body).to.be.equal('Payment methods for cart 9');
             done();
         })
     })
     it("invalid id should return a 404", (done) => {
-        request.get('http://localhost:7865/cart/cow', (error, response, body) => {
-            expect(response.statusCode).to.be.equal(404);
+        request.get('http://localhost:7865/cart/cow', (_error, resp, body) => {
+            expect(resp.statusCode).to.be.equal(404);
             done();
         })
     })
     it('no cart id should return 404', (done) => {
-        request.get('http://localhost:7865/cart/', (error, response, body) => {
-            expect(response.statusCode).to.be.equal(404);
+        request.get('http://localhost:7865/cart/', (_error, resp, body) => {
+            expect(resp.statusCode).to.be.equal(404);
             done();
         })
     })
     it('test login with betty', (done) => {
         request.post('http://localhost:7865/login', {json: {username: 'Betty'}},
-        (error, response, body) => {
-            expect(response.statusCode).to.be.equal(200);
-            expect(body).to.be.equal('Welcome Betty');
+        (_error, resp, body) => {
+            expect(resp.statusCode).to.be.equal(200);
+            expect(body).to.be.equal('Welcome Bettygit');
             done();
         })
     })
     it('test available payments endpoint', (done) => {
-        request.get('http://localhost:7865/available_payments', (error, response, body) => {
-            expect(response.statusCode).to.be.equal(200);
+        request.get('http://localhost:7865/available_payments', (_error, resp, body) => {
+            expect(resp.statusCode).to.be.equal(200);
             expect(JSON.parse(body)).to.be.deep.equal
             ({payment_methods: {credit_cards: true, paypal: false}})
             done();
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,44 +1,43 @@
 const { expect } = require('chai');
 const request = require('request');
-const { response } = require('./api');
 
 describe('api tests', () => {
     it('should test get / returns the correct response', (done) => {
-        request.get('http://localhost:7865', (error, response, body) => {
-            expect(response.statusCode).to.be.equal(200);
+        request.get('http://localhost:7865', (_error, resp, body) => {
+            expect(resp.statusCode).to.be.equal(200);
             expect(body).to.be.equal('Welcome to the payment system');
             done();
         })
     })
     it('validate if id is a number', (done) => {
-        request.get('http://localhost:7865/cart/12', (error, response, body) => {
-            expect(response.statusCode).to.be.equal(200);
+        request.get('http://localhost:7865/cart/12', (_error, resp, body) => {
+            expect(resp.statusCode).to.be.equal(200);
             expect(body).to.be.equal('Payment methods for cart 12');
             done();
         })
     })
     it("invalid id should return a 404", (done) => {
-        request.get('http://localhost:7865/cart/cow', (error, response, body) => {
-            expect(response.statusCode).to.be.equal(404);
+        request.get('http://localhost:7865/cart/cow', (_error, resp, body) => {
+            expect(resp.statusCode).to.be.equal(404);
             done();
         })
     })
     it('no cart id should return 404', (done) => {
-        request.get('http://localhost:7865/cart/', (error, response, body) => {
-            expect(response.statusCode).to.be.equal(404);
+        request.get('http://localhost:7865/cart/', (error, resp, body) => {
+            expect(resp.statusCode).to.be.equal(404);
             done();
         })
     })
     it('test with large cart id number', (done) => {
-        request.get('http://localhost:7865/cart/53', (error, response, body) => {
-            expect(response.statusCode).to.be.equal(200);
+        request.get('http://localhost:7865/cart/53', (error, resp, body) => {
+            expect(resp.statusCode).to.be.equal(200);
             expect(body).to.be.equal('Payment methods for cart 53');
             done();
         })
     })
     it('test to get with negative cart id', (done) => {
-        request.get('http://localhost:7865/cart/-45', (error, response, body) => {
-            expect(response.statusCode).to.be.equal(404);
+        request.get('http://localhost:7865/cart/-45', (error, resp, body) => {
+            expect(resp.statusCode).to.be.equal(404);
             done();
         })
     })
