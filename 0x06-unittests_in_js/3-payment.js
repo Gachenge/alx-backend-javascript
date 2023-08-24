fc832d99e0772704710ac61999cb3fc878bd1d20
Gachenge <gachenge1@gmail.com>
2023-08-24 18:39:10 +0300

    corrections for package.json

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -2,7 +2,7 @@ const Utils = require('./utils');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping){
     const tots = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-    console.log(`The total is: ${tots}`)
+    console.log(`The total is: ${tots}`);
 }
 
-module.exports = sendPaymentRequestToApi
+module.exports = sendPaymentRequestToApi;
